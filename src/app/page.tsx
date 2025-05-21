b6988cd4951888f6f4a0fce376e36ed88e9b416e
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-05-21 21:41:35 +0530

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
-  return <main>home</main>;
+  const [helpTab, setHelpTab] = useState(false);
+
+  return (
+    <main className="flex min-h-dvh gap-2 p-2">
+      <section className="w-full max-w-xs rounded bg-emerald-800"></section>
+      <section className="flex flex-1 items-center justify-center rounded bg-emerald-200">
+        <button
+          onClick={() => setHelpTab(!helpTab)}
+          className="rounded bg-emerald-800 p-2"
+        >
+          AI
+        </button>
+      </section>
+      {helpTab && (
+        <section className="w-full max-w-xs rounded bg-emerald-800"></section>
+      )}
+    </main>
+  );
 }
