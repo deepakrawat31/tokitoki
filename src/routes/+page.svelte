cfaaa2e44c9b61a4ae259ef1caaa173a1ff20ccd
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-05-26 23:04:33 +0530

    changed things to svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let screenWidth: number | null = $state(null);
+</script>
+
+<svelte:window bind:innerWidth={screenWidth} />
+
+<main class="flex min-h-dvh gap-2 p-2">
+	<div
+		class="flex w-full max-w-2xl flex-col justify-center gap-8 rounded bg-neutral-800 p-4 sm:max-w-xs"
+	>
+		<h1 class="text-xl font-bold uppercase">tokitoki</h1>
+		<form action="" class="flex flex-col gap-8"></form>
+	</div>
+	{#if screenWidth !== null && screenWidth >= 640}
+		<div class="flex-1 rounded bg-neutral-800"></div>
+	{/if}
+</main>
