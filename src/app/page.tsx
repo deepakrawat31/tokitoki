6c459e9e990c0a5f22ae341f1ae46e5f11b84854
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-06-07 23:56:30 +0530

    small update on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 export default function Home() {
   return (
     <main className="flex min-h-dvh items-center justify-center gap-8 p-4">
-      <div className="flex w-full max-w-md flex-col items-center justify-center gap-4">
+      <div className="flex h-[60dvh] w-full max-w-md flex-col items-start justify-center gap-4">
+        <p className="text-sm uppercase md:text-base">
+          "Conversations without borders."
+        </p>
         <h1 className="text-2xl font-bold text-emerald-400 uppercase md:text-6xl">
           tokitoki
         </h1>
-        <p></p>
       </div>
       <div className=""></div>
     </main>
